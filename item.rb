ca0d7652a0f0f9af0d0c19358056836008053461
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-07 05:20:20 -0700

    Create one-to-many relationships

--- item.rb
+++ item.rb
@@ -1,35 +1,44 @@
 require 'date'
 
 class Item
-    attr_accessor :genre
-    attr_accessor :author
-    attr_accessor :source
-    attr_accessor :label
-    attr_accessor :publish_date
+    # attr_accessor :genre
+    # attr_accessor :author
+    # attr_accessor :source
+    # attr_accessor :label
+    attr_reader :publish_date
 
-    def initialize(id, genre, author, source, label, publish_date: Date.today, archived: false)
+    attr_accessor :archived
+
+    def initialize(id, publish_date: Date.today, archived: false)
         @id = id
-        @genre = genre
-        @author = author
-        @source = source
-        @label = label
+        # @genre = Genre.new()
+        # @author = Author.new()
+        # @source = Source.new()
+        # @label = Label.new()
         @publish_date = publish_date.strftime('%Y-%m-%d')
         @archived = archived
     end
     
     def move_to_archive
-        
+        @archived = true if can_be_archived?
+
     end
     
     private
-    def can_be_archived?
-        
+    def can_be_archived? 
+        year, month, day = @publish_date.split('-').map(&:to_i)
+        return true if (Date.today - Date.new(year, month, day))/365.25 > 10
+        false
     end
 end
 
-item = Item.new(1, 'Sci-Fi', 'Jack' , 'Local library', 'B1', publish_date: Date.new(2023, 8, 12), archived: true )
-p item.genre
-p item.author
-p item.source
-p item.label
+item = Item.new(1, publish_date: Date.new(2013, 8, 6), archived: false )
+# p item.genre
+# p item.author
+# p item.source
+# p item.label
+
 p item.publish_date
+p item.archived
+p item.move_to_archive
+p item.archived
