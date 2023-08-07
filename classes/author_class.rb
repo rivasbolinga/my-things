b1b766a8189e7578571f36eb33b49958d4060dde
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-07 18:36:48 +0200

    Create add_item method

--- classes/author_class.rb
+++ classes/author_class.rb
@@ -1,10 +1,15 @@
 class Author
+  attr_reader :id
+  attr_accessor :first_name, :last_name, :items
   def initialize(id, first_name, last_name, items)
-    @id = id
+    @id = Random.rand(1...100)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
-  
+ def add_item(item)
+    @items << item
+    item.add_author(self) unless item.author == (self)
+  end
 end
