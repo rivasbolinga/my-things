5ef5e2f688461bd05776814d68762ddb0b1a2062
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-07 18:38:42 +0200

    Fix linters

--- classes/author_class.rb
+++ classes/author_class.rb
@@ -1,14 +1,15 @@
 class Author
   attr_reader :id
   attr_accessor :first_name, :last_name, :items
-  def initialize(id, first_name, last_name, items)
+
+  def initialize(_id, first_name, last_name, _items)
     @id = Random.rand(1...100)
     @first_name = first_name
     @last_name = last_name
     @items = []
   end
 
- def add_item(item)
+  def add_item(item)
     @items << item
     item.add_author(self) unless item.author == (self)
   end
--- classes/game_class.rb
+++ classes/game_class.rb
@@ -1,6 +1,8 @@
 class Game < Item
   attr_accessor :multiplayer, :last_played_at
+
   def initialize(multiplayer, last_played_at)
+    super
     @multiplayer = multiplayer
     @last_played_at = last_played_at
   end
