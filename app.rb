29dacb1d7ef3760b168a63320c7510b13b91768f
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-07 16:37:13 +0200

    Fix linters and run tests

--- app.rb
+++ app.rb
@@ -4,48 +4,81 @@ class App
   def initialize
     @ui = UI.new
   end
-  
- def run
+
+  def run
     @ui.welcome
     loop do
       @ui.options
-      option = gets.chomp.to_i
-      apply_option(option)
+      option = gets.chomp
+      break if option.downcase == 'exit'
+
+      apply_option(option.to_i)
+    end
+    exit_app
+  end
+
+  # To be completed later on ↓
+  def apply_option(option)
+    actions = {
+      1 => method(:list_all_books),
+      2 => method(:list_all_music_albums),
+      3 => method(:list_all_games),
+      4 => method(:list_all_genres),
+      5 => method(:list_all_labels),
+      6 => method(:list_all_authors),
+      7 => method(:create_book),
+      8 => method(:create_music_album),
+      9 => method(:create_game),
+      10 => method(:exit_app)
+    }
+
+    action = actions[option]
+    if action
+      action.call
+    else
+      puts '❌ Sorry! You have to select one of the options'
     end
   end
 
-  #To be completed later on ↓
-  def apply_option(option)
-    case option
-    when 1
-      puts 'You have selected 1 - List all books'
-    when 2
-      puts 'You have selected 2 - List all music albums'
-    when 3
-      puts 'You have selected 3 - List all games'
-    when 4
-      puts 'You have selected 4 - List all genres'
-    when 5
-      puts 'You have selected 5 - List all labels'
-    when 6
-      puts 'You have selected 6 - List all authors'
-    when 7
-      puts 'You have selected 7 - Create a book'
-    when 8
-      puts 'You have selected 8 - Create a music album'
-    when 9
-      puts 'You have selected 9 - Create a game'
-    when 10
-      exit_app
-    else 
-      puts 'Sorry! You have to select one of the options'
-    end
+  def list_all_books
+    puts 'You have selected 1 - List all books'
+  end
+
+  def list_all_music_albums
+    puts 'You have selected 2 - List all music albums'
+  end
+
+  def list_all_games
+    puts 'You have selected 3 - List all games'
+  end
+
+  def list_all_genres
+    puts 'You have selected 4 - List all genres'
+  end
+
+  def list_all_labels
+    puts 'You have selected 5 - List all labels'
+  end
+
+  def list_all_authors
+    puts 'You have selected 6 - List all authors'
+  end
+
+  def create_book
+    puts 'You have selected 7 - Create a book'
+  end
+
+  def create_music_album
+    puts 'You have selected 8 - Create a music album'
+  end
+
+  def create_game
+    puts 'You have selected 9 - Create a game'
   end
 
   def exit_app
-    puts 'Exiting the application....' 
-    puts 'Goodbye!'
+    puts 'Exiting the application....'
+    puts 'Goodbye!👋🏼'
     exit
   end
-
 end
