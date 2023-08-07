827e9e7b9923df38b98d6ac084965f44c9d2f4fd
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-07 18:20:33 +0200

    Add attr accessors for Game class and create Author class

---
+++ classes/author_class.rb
@@ -0,0 +1,10 @@
+class Author
+  def initialize(id, first_name, last_name, items)
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+
+  
+end
--- classes/game_class.rb
+++ classes/game_class.rb
@@ -1,4 +1,5 @@
 class Game < Item
+  attr_accessor :multiplayer, :last_played_at
   def initialize(multiplayer, last_played_at)
     @multiplayer = multiplayer
     @last_played_at = last_played_at
