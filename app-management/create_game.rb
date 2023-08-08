480fdae00c4fecdef502c4f2e0751ec83b57767d
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-08 21:40:46 +0200

    Add case for invalid date format

--- app-management/create_game.rb
+++ app-management/create_game.rb
@@ -1,26 +1,32 @@
 require_relative '../classes/game_class'
+require 'date'
+
 class CreateGame
   def initialize
     @games = []
   end
-   def create_game
-   print 'Multiplayer:'
+
+  def create_game
+    print 'Multiplayer: '
     multiplayer = gets.chomp
-    # print 'Publish Date[YYYY-MM-DD]: '
-    # publish_date_input = gets.chomp
-    # publish_date = parse_date_input(publish_date_input)
-    print 'Last time played[YYYY-MM-DD]: '
-    last_played_at_input = gets.chomp
-    last_played_at = parse_date_input(last_played_at_input)
-    new_game = Game.new(multiplayer, last_played_at, )
+    last_played_at = nil  # Initialize last_played_at to nil
+
+    loop do
+      print 'Last time played [YYYY-MM-DD]: '
+      last_played_at_input = gets.chomp
+      last_played_at = parse_date_input(last_played_at_input)
+      break if last_played_at
+    end
+
+    new_game = Game.new(multiplayer, last_played_at)
     @games.push(new_game)
-    puts '🟢 Game created succesfully!!'
+    puts '🟢 Game created successfully!!'
   end
 
   def parse_date_input(date_input)
     Date.parse(date_input)
   rescue ArgumentError
     puts '🔴 Invalid date format. Please use YYYY-MM-DD.'
-    retry
+    nil
   end
 end
