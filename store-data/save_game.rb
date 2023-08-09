19ffa15a47e5cac8f6f2be62c38afd49bd44f402
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-09 07:52:01 +0200

    Store game succesfully

--- store-data/save_game.rb
+++ store-data/save_game.rb
@@ -1,5 +1,18 @@
+require 'json'
+
 class StoreGame
-  def store_game(hashed)
-    puts hashed
+  def store_game(hashed_data)
+    json_data = hashed_data.map do |game|
+      {
+        multiplayer: game[:multiplayer],
+        last_played_at: game[:last_played_at]
+      }
+    end
+
+    File.open('./data-files/games.json', 'w') do |file|
+      file.write(JSON.pretty_generate(json_data))
+    end
+
+    puts '📝 Games data saved to games.json!'
   end
 end
