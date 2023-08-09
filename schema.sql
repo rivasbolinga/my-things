3e9b4c677b74521ff3f819c39fc4eb8dc082c7e5
Momo-87 <momochristian87@gmail.com>
2023-08-09 20:36:02 +0100

    Implement SQL code for move_to_archive and can_be_archived? methods

--- schema.sql
+++ schema.sql
@@ -12,6 +12,17 @@ CREATE TABLE items (
 
 CREATE TABLE music_albums (
   item_id INTEGER PRIMARY KEY,
+  publish_date DATE,
+  archived BOOLEAN,
   on_spotify BOOLEAN,
   FOREIGN KEY (item_id) REFERENCES items(id)
+  FOREIGN key (genre_id) REFERENCES genres(id)
 );
+
+UPDATE items
+SET archived = true
+WHERE AGE(current_date, publish_date) >= (10 * 365.25);
+
+UPDATE music_albums
+SET archived = true
+WHERE AGE(current_date, publish_date) >= (10 * 365.25);
