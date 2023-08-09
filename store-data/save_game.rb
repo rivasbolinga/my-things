f6d39576a3f1ad7e2541c49e223ac6d9837e122a
Andrea Rivas <andrearivas4cinfor@gmail.com>
2023-08-09 07:54:19 +0200

    Error message in case games don't get store successfully

--- store-data/save_game.rb
+++ store-data/save_game.rb
@@ -2,17 +2,26 @@ require 'json'
 
 class StoreGame
   def store_game(hashed_data)
-    json_data = hashed_data.map do |game|
-      {
-        multiplayer: game[:multiplayer],
-        last_played_at: game[:last_played_at]
-      }
-    end
+    begin
+      json_data = hashed_data.map do |game|
+        {
+          multiplayer: game[:multiplayer],
+          last_played_at: game[:last_played_at]
+        }
+      end
 
-    File.open('./data-files/games.json', 'w') do |file|
-      file.write(JSON.pretty_generate(json_data))
-    end
+      File.open('./data-files/games.json', 'w') do |file|
+        file.write(JSON.pretty_generate(json_data))
+      end
 
-    puts '📝 Games data saved to games.json!'
+      puts '📝 Games data saved to games.json!'
+    rescue StandardError => e
+      puts "🚨 An error occurred while saving the games data: #{e.message}"
+    end
   end
 end
+
+
+
+
+
