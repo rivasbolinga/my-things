cbfece801345703e928f2afcbce1dbb0e4f8ce03
Momo-87 <momochristian87@gmail.com>
2023-08-09 06:31:42 +0100

    Implement SQL code to create items table and implement one-to-many relationship with genres table

--- schema.sql
+++ schema.sql
@@ -2,3 +2,10 @@ CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE items (
+  id SERIAL PRIMARY KEY,
+  genre_id INTEGER REFERENCES genres(id),
+  publish_date DATE,
+  archived BOOLEAN
+);
