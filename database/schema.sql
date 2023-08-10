12cd76d735679c4320754fee40057f350d19d42b
mohametalmeari <mohamet.almeari@gmail.com>
2023-08-10 07:13:19 -0700

    Update Schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -2,15 +2,16 @@ CREATE TABLE items (
   id INT PRIMARY KEY NOT NULL,
   publish_date DATE,
   archived BOOLEAN DEFAULT FALSE,
-  label_id INT,
-
-  -- more one-to-many connections 
+  label_id INTEGER REFERENCES label(id),
+  author_id INTEGER REFERENCES authors(id),
+  genre_id INTEGER REFERENCES genres(id),
 );
 
 CREATE TABLE book (
   item_id INT PRIMARY KEY NOT NULL,
   publisher VARCHAR(100),
-  cover_state VARCHAR(100)
+  cover_state VARCHAR(100),
+  FOREIGN KEY (item_id) REFERENCES items(id)
 );
 
 CREATE TABLE label (
@@ -19,11 +20,42 @@ CREATE TABLE label (
   color VARCHAR(100)
 );
 
-ALTER TABLE items ADD CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES label (id);
-ALTER TABLE book ADD CONSTRAINT fk_item FOREIGN KEY (item_id) REFERENCES items (id);
+CREATE TABLE game (
+  item_id INTEGER PRIMARY KEY,
+  multiplayer varchar(100),
+  last_played_at DATE,
+  FOREIGN KEY (item_id) REFERENCES items(id)
+);
+
+CREATE TABLE authors (
+  id  INTEGER PRIMARY KEY,
+  first_name varchar(100),
+  last_name varchar(100)
+);
+
+CREATE TABLE music_albums (
+  item_id INTEGER PRIMARY KEY,
+  on_spotify BOOLEAN,
+  FOREIGN KEY (item_id) REFERENCES items(id)
+);
+
+CREATE TABLE genres (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL
+);
 
 UPDATE items 
 SET archived = TRUE
 WHERE AGE(CURRENT_DATE, publish_date) > INTERVAL '10 years'
 OR id IN (SELECT item_id FROM book WHERE cover_state = 'bad');
+
+UPDATE items 
+SET archived = TRUE
+WHERE AGE(CURRENT_DATE, publish_date) > INTERVAL '10 years'
+AND id IN (SELECT item_id FROM game WHERE AGE(CURRENT_DATE, last_played_at) > INTERVAL '2 years');
+
+UPDATE items
+SET archived = TRUE
+WHERE AGE(CURRENT_DATE, publish_date) > INTERVAL '10 years'
+AND id IN (SELECT item_id FROM music_albums WHERE on_spotify = TRUE);
 
